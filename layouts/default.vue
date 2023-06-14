440afdd93821c9c920f819561fc5fafe6bda3807
Neil-Lin <neillin1023@gmail.com>
2023-06-14 21:59:05 +0800

    fix: noscript

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="layout">
     <noscript class="noscript">
-      Your browser does not support JavaScript! 您的瀏覽器不支援 JavaScript
-      功能，若網頁功能無法正常使用時，請開啟瀏覽器 JavaScript 狀態。
+      您的瀏覽器不支援 JavaScript 功能，若網頁功能無法正常使用時，請開啟瀏覽器 JavaScript 狀態。
+      Your browser does not support JavaScript!
     </noscript>
     <TheHeader />
     <slot />
