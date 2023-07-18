938d8fd422b74cbbd95a70134ffbcb26bdba20e3
Neil-Lin <neillin1023@gmail.com>
2023-07-18 16:00:47 +0800

    feat: Add Pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/html-validator', 'nuxt-swiper'],
+  modules: ['@nuxtjs/html-validator', 'nuxt-swiper', '@pinia/nuxt'],
   htmlValidator: {
     usePrettier: true
+  },
+  imports: {
+    dirs: ['stores']
   },
   runtimeConfig: {
     public: {
