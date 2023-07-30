af790e94f8572b7eaca1e28bfced35d56ba33318
Neil-Lin <neillin1023@gmail.com>
2023-07-30 23:38:28 +0800

    fix: html2canvas des

--- pages/screenshot.vue
+++ pages/screenshot.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    還沒支援 oklch，若需要此功能，請使用 scss 寫法。
+    還沒支援 oklch，若需要此功能，請使用 CSS3 color function 寫法。
     <button type="button" @click="handleScreenshot()">點我截圖！</button>
     <div v-if="loadingScreenshotImg">載入螢幕截圖中...</div>
     <img v-if="screenshot" class="screenshot" :src="screenshot" alt="螢幕截圖" />
